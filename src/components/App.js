b0fb9589e61960a94de34140e8d0f80ba193f65f
a-sapon <sapon.anya@gmail.com>
2020-07-17 14:55:25 +0300

    add operations

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,47 @@
 import React, { useState } from 'react';
 import Output from './Output/Output';
-import styles from './App.module.css';
 import KeyPad from './KeyPad/KeyPad';
+import styles from './App.module.css';
 
 const App = () => {
   const [output, setOutput] = useState('0');
   const [prevNumber, setPrevNumber] = useState('');
-  const [currentNumber, setCurrentNumber] = useState('');
-  const [operatorNumber, setOperatorNumber] = useState('');
+  const [operator, setOperator] = useState('');
 
-  const addDigit = (e) => {
+  const [activePlus, setActivePlus] = useState(false);
+  const [activeMinus, setActiveMinus] = useState(false);
+  const [activeMultiply, setActiveMultiply] = useState(false);
+  const [activeDivide, setActiveDivide] = useState(false);
+
+  const addToOutput = (e) => {
     e.persist();
     if (output === '0') {
       setOutput(e.target.innerText);
+    } else if (operator !== '') {
+      setOutput((prev) => prev + e.target.innerText);
     } else {
       setOutput((prev) => prev + e.target.innerText);
     }
   };
 
   const clear = () => {
+    setPrevNumber('');
+    setOperator('');
+    setActivePlus(false);
+    setActiveMinus(false);
+    setActiveMultiply(false);
+    setActiveDivide(false);
+  };
+
+  const clearAllBtn = () => {
     setOutput('0');
+    clear();
   };
 
   const addZero = (e) => {
     e.persist();
     if (output === '0') {
-      clear();
+      return;
     } else {
       setOutput((prev) => prev + e.target.innerText);
     }
@@ -33,9 +49,12 @@ const App = () => {
 
   const addDecimal = (e) => {
     e.persist();
-    if (output.split('').includes(e.target.innerText)) {
-    } else {
-      setOutput((prev) => prev + e.target.innerText);
+    if (!String(output).split('').includes(e.target.innerText)) {
+      if (output === '') {
+        setOutput('0' + e.target.innerText);
+      } else {
+        setOutput((prev) => prev + e.target.innerText);
+      }
     }
   };
 
@@ -47,20 +66,97 @@ const App = () => {
     }
   };
 
+  const getPercentage = () => {
+    setOutput(+output / 100);
+  };
+
+  const add = () => {
+    setPrevNumber(parseFloat(output));
+    setOperator('plus');
+    setOutput('');
+    setActivePlus(true);
+  };
+
+  const subtract = () => {
+    setPrevNumber(parseFloat(output));
+    setOperator('minus');
+    setOutput('');
+    setActiveMinus(true);
+  };
+
+  const multiply = () => {
+    setPrevNumber(parseFloat(output));
+    setOperator('multiply');
+    setOutput('');
+    setActiveMultiply(true);
+  };
+
+  const divide = () => {
+    setPrevNumber(parseFloat(output));
+    setOperator('divide');
+    setOutput('');
+    setActiveDivide(true);
+  };
+
   const calculate = () => {
-    
+    if (output !== '') {
+      switch (operator) {
+        case 'plus':
+          setOutput(prevNumber + parseFloat(output));
+          break;
+        case 'minus':
+          setOutput(prevNumber - parseFloat(output));
+          break;
+        case 'multiply':
+          setOutput(prevNumber * parseFloat(output));
+          break;
+        case 'divide':
+          setOutput(prevNumber / parseFloat(output));
+          break;
+        default:
+          break;
+      }
+    } else {
+      switch (operator) {
+        case 'plus':
+          setOutput(prevNumber + prevNumber);
+          break;
+        case 'minus':
+          setOutput(prevNumber - prevNumber);
+          break;
+        case 'multiply':
+          setOutput(prevNumber * prevNumber);
+          break;
+        case 'divide':
+          setOutput(prevNumber / prevNumber);
+          break;
+        default:
+          break;
+      }
+    }
+
+    clear();
   };
 
   return (
     <div className={styles.container}>
-      <Output result={output} />
+      <Output result={output || prevNumber} />
       <KeyPad
-        addDigit={addDigit}
-        clear={clear}
+        addToOutput={addToOutput}
+        clear={clearAllBtn}
         addZero={addZero}
         addDecimal={addDecimal}
         makePositiveOrNegative={makePositiveOrNegative}
+        getPercentage={getPercentage}
+        add={add}
+        subtract={subtract}
+        multiply={multiply}
+        divide={divide}
         calculate={calculate}
+        activePlus={activePlus}
+        activeMinus={activeMinus}
+        activeMultiply={activeMultiply}
+        activeDivide={activeDivide}
       />
     </div>
   );
--- src/components/KeyPad/KeyPad.js
+++ src/components/KeyPad/KeyPad.js
@@ -1,35 +1,51 @@
 import React from 'react';
 import styles from './KeyPad.module.css';
 
-const KeyPad = ({ addDigit, clear, addZero, addDecimal, makePositiveOrNegative, calculate }) => (
+const KeyPad = ({
+  addToOutput,
+  clear,
+  addZero,
+  addDecimal,
+  makePositiveOrNegative,
+  getPercentage,
+  add,
+  subtract,
+  multiply,
+  divide,
+  calculate,
+  activePlus,
+  activeMinus,
+  activeMultiply,
+  activeDivide
+}) => (
   <div className={styles.keypad}>
     <div className={styles.digits}>
-      <button onClick={clear} type='button'>AC</button>
-      <button onClick={makePositiveOrNegative} type='button'>+/-</button>
-      <button type='button'>%</button>
+      <button onClick={clear} className={styles.digitsBtn} type='button'>AC</button>
+      <button onClick={makePositiveOrNegative} className={styles.digitsBtn} type='button'>+/-</button>
+      <button onClick={getPercentage} className={styles.digitsBtn} type='button'>%</button>
 
-      <button onClick={addDigit} type='button'>7</button>
-      <button onClick={addDigit} type='button'>8</button>
-      <button onClick={addDigit} type='button'>9</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>7</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>8</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>9</button>
 
-      <button onClick={addDigit} type='button'>4</button>
-      <button onClick={addDigit} type='button'>5</button>
-      <button onClick={addDigit} type='button'>6</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>4</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>5</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>6</button>
 
-      <button onClick={addDigit} type='button'>1</button>
-      <button onClick={addDigit} type='button'>2</button>
-      <button onClick={addDigit} type='button'>3</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>1</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>2</button>
+      <button onClick={addToOutput} className={styles.digitsBtn} type='button'>3</button>
 
       <button onClick={addZero} type='button' className={styles.zero}>0</button>
-      <button onClick={addDecimal} type='button'>.</button>
+      <button onClick={addDecimal} className={styles.digitsBtn} type='button'>.</button>
     </div>
 
     <div className={styles.operations}>
-      <button type='button'>&#247;</button>
-      <button type='button'>x</button>
-      <button type='button'>-</button>
-      <button type='button'>+</button>
-      <button onClick={calculate} type='button'>=</button>
+      <button onClick={divide} className={activeDivide ? `${styles.operationsBtn} ${styles.active}` : `${styles.operationsBtn}`} type='button'>&#247;</button>
+      <button onClick={multiply} className={activeMultiply ? `${styles.operationsBtn} ${styles.active}` : `${styles.operationsBtn}`} type='button'>x</button>
+      <button onClick={subtract} className={activeMinus ? `${styles.operationsBtn} ${styles.active}` : `${styles.operationsBtn}`} type='button'>-</button>
+      <button onClick={add} className={activePlus ? `${styles.operationsBtn} ${styles.active}` : `${styles.operationsBtn}`} type='button'>+</button>
+      <button onClick={calculate} className={styles.operationsBtn} type='button'>=</button>
     </div>
   </div>
 );
