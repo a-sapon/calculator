ce6f4f6ee2dd60b99d402b66e16066a940c992be
a-sapon <sapon.anya@gmail.com>
2020-07-17 14:58:10 +0300

    Updates

---
+++ precache-manifest.106b292978d49b4007851f3387eb5256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "475381142bae6ca2c6a51639c6295c83",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "c519a6dd4f27c0c854b7",
+    "url": "/calculator/static/css/main.dbdb70f8.chunk.css"
+  },
+  {
+    "revision": "43ff07d7d82f8e8b4b6e",
+    "url": "/calculator/static/js/2.f7fcd12c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator/static/js/2.f7fcd12c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c519a6dd4f27c0c854b7",
+    "url": "/calculator/static/js/main.b8424661.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
